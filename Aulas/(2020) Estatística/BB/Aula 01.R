da21b2cf93c3c246febe317735de6c7787331535
Leticia Raposo <raposo@peb.ufrj.br>
2020-03-13 14:41:28 -0300

    aula 01 de R script

---
+++ Aulas/(2020) Estatística/BB/Aula 01.R
@@ -0,0 +1,26 @@
+#Descobrindo um diretorio de trabalho:
+getwd()
+
+#Definindo um diretorio de trabalho:
+setwd("C:\\Users\\Leticia\\Downloads\\bioest")
+
+#Calculando uma raiz quadrada:
+sqrt(100)
+
+#Carregando um banco de dados do R:
+data("iris")
+iris
+
+#Olhando apenas uma variavel (banco$variavel)
+iris$Species
+iris$Sepal.Length
+
+#Criando objetos (<-)
+especies <- iris$Species 
+
+bancoiris <- iris
+
+resultado <- sqrt(2374563829)
+
+
+
